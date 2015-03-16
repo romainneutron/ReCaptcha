7b54ad069182b9aaea6e2cb2df7a058da407fc7b
dmouse <dmousex@gmail.com>
2015-03-16 16:26:33 -0600

    set the docblock

--- src/Neutron/ReCaptcha/ReCaptcha.php
+++ src/Neutron/ReCaptcha/ReCaptcha.php
@@ -10,10 +10,26 @@ use Symfony\Component\HttpFoundation\Request;
 /** @see https://developers.google.com/recaptcha/docs/customization */
 class ReCaptcha
 {
+    /**
+     * @var ClientInterface
+     */
     private $client;
+
+    /**
+     * @var string
+     */
     private $publicKey;
+
+    /**
+     * @var string
+     */
     private $privateKey;
 
+    /**
+     * @param ClientInterface $client
+     * @param string          $publicKey
+     * @param string          $privateKey
+     */
     public function __construct(ClientInterface $client, $publicKey, $privateKey)
     {
         $this->client = $client;
@@ -21,16 +37,39 @@ class ReCaptcha
         $this->publicKey = $publicKey;
     }
 
+    /**
+     * @return bool
+     */
     public function isSetup()
     {
         return '' !== trim($this->privateKey) && '' !== trim($this->publicKey);
     }
 
-    public function bind(Request $request, $challenge = 'recaptcha_challenge_field', $response = 'recaptcha_response_field')
+    /**
+     * @param Request $request
+     * @param string  $challenge
+     * @param string  $response
+     * @return Response
+     */
+    public function bind(
+        Request $request,
+        $challenge = 'recaptcha_challenge_field',
+        $response = 'recaptcha_response_field'
+    )
     {
-        return $this->checkAnswer($request->getClientIp(), $request->request->get($challenge), $request->request->get($response));
+        return $this->checkAnswer(
+            $request->getClientIp(),
+            $request->request->get($challenge),
+            $request->request->get($response)
+        );
     }
 
+    /**
+     * @param string $ip
+     * @param string $challenge
+     * @param string $response
+     * @return Response
+     */
     public function checkAnswer($ip, $challenge, $response)
     {
         if ('' === trim($ip)) {
@@ -61,13 +100,25 @@ class ReCaptcha
         return new Response(false, isset($data[1]) ? $data[1] : null);
     }
 
+    /**
+     * @return string
+     */
     public function getPublicKey()
     {
         return $this->publicKey;
     }
 
+    /**
+     * @param string $publicKey
+     * @param string $privateKey
+     * @return ReCaptcha
+     */
     public static function create($publicKey, $privateKey)
     {
-        return new ReCaptcha(new Client('https://www.google.com/recaptcha/api'), $publicKey, $privateKey);
+        return new ReCaptcha(
+            new Client('https://www.google.com/recaptcha/api'),
+            $publicKey,
+            $privateKey
+        );
     }
 }
